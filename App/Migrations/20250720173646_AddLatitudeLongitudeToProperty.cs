a8244800f4d6e8bd5e37ea23b511be11a3526efb
Vlad Syzov <hizes5050@gmail.com>
2025-07-21 02:06:00 +0200

    Added latitude and longtitude for googlemap

---
+++ App/Migrations/20250720173646_AddLatitudeLongitudeToProperty.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLatitudeLongitudeToProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Properties",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Properties",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("efdaa81f-9f1e-4e3d-8050-245cca05bf4b"),
+                column: "Time",
+                value: new DateTime(2025, 7, 20, 17, 36, 46, 119, DateTimeKind.Utc).AddTicks(7050));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Properties");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("efdaa81f-9f1e-4e3d-8050-245cca05bf4b"),
+                column: "Time",
+                value: new DateTime(2025, 7, 13, 17, 45, 56, 541, DateTimeKind.Utc).AddTicks(660));
+        }
+    }
+}
